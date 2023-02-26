8e04ee21261543afcd2b8fc25d1142318e959f26
Nick Barnard <n.barnard@ctidigital.com>
2023-02-26 17:40:56 +0000

    Add mousePosition atoms

--- src/atoms/atoms.ts
+++ src/atoms/atoms.ts
@@ -6,3 +6,10 @@ export const windowDimensionsAtom = atom((get) => ({
   w: get(windowWidthAtom),
   h: get(windowHeightAtom),
 }));
+
+export const mouseXAtom = atom<number>(window.innerWidth / 2);
+export const mouseYAtom = atom<number>(window.innerHeight / 2);
+export const mouseXYAtom = atom((get) => ({
+  x: get(mouseXAtom),
+  y: get(mouseYAtom),
+}));
