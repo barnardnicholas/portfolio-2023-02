268f3e203692ab1b85e2c66a2ebb41d9fb810bd8
Nick Barnard <n.barnard@ctidigital.com>
2023-03-07 16:41:49 +0000

    Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from '@pages/home/Home';
 import BackgroundContainer from '@components/background/BackgroundContainer';
 import useUpdateWindowDimensions from '@hooks/useUpdateWindowDimensions';
 import { Container } from '@mui/material';
+import Footer from '@components/footer/Footer';
 
 function App() {
   useUpdateWindowDimensions();
@@ -15,6 +16,7 @@ function App() {
           <BackgroundContainer />
           <Home />
         </Container>
+        <Footer />
       </div>
     </Suspense>
   );
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ContactLinks from '@components/contactLinks/ContactLinks';
+import { Box, Container, Link, Typography, useTheme } from '@mui/material';
+import ContentBlock from '@components/layout/ContentBlock';
+import { addOpacityToColor } from '@utils/utils';
+
+const Footer: React.FC = () => {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        backgroundColor: addOpacityToColor(theme.palette.background.paper, 0.66),
+        pt: 2,
+        pb: 2,
+        borderTop: '1px solid rgba(255, 255, 255, 0.12)',
+        boxShadow: `0 -2px 6px 1px ${addOpacityToColor('#000000', 0.5)}`,
+      }}
+    >
+      <Container>
+        <ContentBlock sx={{ mb: 0 }}>
+          <Box
+            className="con"
+            sx={{ flex: 1, display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}
+          >
+            <Box sx={{ flex: 1, pr: 1 }}>
+              <Typography sx={{ mb: 0.5 }} variant="body1">
+                All design & dev by Nick Barnard 2023
+              </Typography>
+              <Link href="https://github.com/barnardnicholas/portfolio-2023-02" target="blank">
+                Check my source code
+              </Link>
+            </Box>
+            <ContactLinks />
+          </Box>
+        </ContentBlock>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
