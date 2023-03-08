448338641c24ab1eaba0b4885bd234d6c47131b2
Nick Barnard <n.barnard@ctidigital.com>
2023-03-08 12:15:24 +0000

    Fix markdown bug

--- src/pages/home/Portfolio.tsx
+++ src/pages/home/Portfolio.tsx
@@ -50,7 +50,7 @@ function Portfolio() {
                 storytelling using InkJS library. Play thorugh <em>The Intercept</em>, Inkle&apos;s
                 short demo choose-your-own-adventure-story, and see the debug information as you
                 play. I may turn this into an NPM package for people to use in the future. For now
-                it's a prototype.
+                it&apos;s a prototype.
               </Typography>
               <Typography variant="body1" sx={{ mb: 3 }}>
                 <Link href="https://github.com/barnardnicholas/ink-react" target="blank">
