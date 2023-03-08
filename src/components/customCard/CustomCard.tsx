811a31c2e688e29b2c0ed6f2107978e415e69601
Nick Barnard <n.barnard@ctidigital.com>
2023-03-08 12:50:06 +0000

    Add rise on hover prop to custom card

--- src/components/customCard/CustomCard.tsx
+++ src/components/customCard/CustomCard.tsx
@@ -1,4 +1,3 @@
-import { standardTransitions } from '@/theme/constants';
 import { Card, SxProps, Theme, useTheme } from '@mui/material';
 import { addOpacityToColor } from '@utils/utils';
 import React, { MouseEventHandler, PropsWithChildren } from 'react';
@@ -7,9 +6,12 @@ const CustomCard: React.FC<CustomCardProps> = ({
   sx = {},
   variant = 'outlined',
   onClick = undefined,
+  riseOnHover,
   children,
 }) => {
   const theme = useTheme();
+  const transition = `background-color 500ms ease, color 500ms ease, border-color 500ms ease, transform 200ms ease`;
+  const isMobile = window.innerWidth <= theme.breakpoints.values.sm;
   return (
     <Card
       sx={{
@@ -19,7 +21,15 @@ const CustomCard: React.FC<CustomCardProps> = ({
         justifyContent: 'center',
         backgroundColor: addOpacityToColor(theme.palette.background.paper, 0.66),
         cursor: !!onClick ? 'pointer' : 'inherit',
-        transition: standardTransitions(theme),
+        transform: `scale(1)`,
+        '&:hover':
+          riseOnHover && !isMobile
+            ? {
+                transform: `scale(102%)`,
+                boxShadow: `0px 6px 12px 4px rgba(0,0,0,0.2)`,
+              }
+            : {},
+        transition,
         ...sx,
       }}
       variant={variant}
@@ -34,6 +44,7 @@ interface CustomCardProps extends PropsWithChildren {
   sx?: SxProps<Theme>;
   variant?: 'elevation' | 'outlined';
   onClick?: MouseEventHandler<HTMLDivElement>;
+  riseOnHover?: boolean;
 }
 
 export default CustomCard;
