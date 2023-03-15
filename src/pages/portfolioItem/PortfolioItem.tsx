4dab612345641409bd48d440242436c8cda258c6
Nick Barnard <n.barnard@ctidigital.com>
2023-03-15 13:50:47 +0000

    Add image to PortfolioItem

--- src/pages/portfolioItem/PortfolioItem.tsx
+++ src/pages/portfolioItem/PortfolioItem.tsx
@@ -6,9 +6,13 @@ import { useParams } from 'react-router-dom';
 import PageContainer from '@components/layout/pageContainer/PageContainer';
 import Breadcrumb from '@components/breadcrumb/Breadcrumb';
 import portfolioItems from '@constants/portfolioItems';
+import { addOpacityToColor, getPortfolioImagePathFromSlug } from '@utils/utils';
+import { useAtom } from 'jotai';
+import { themeAtom } from '@/atoms/atoms';
 
 function PortfolioItem() {
   const theme = useTheme();
+  const [currentTheme] = useAtom(themeAtom);
   const { slug } = useParams<{ slug: string }>();
 
   const portfolioItem = portfolioItems[slug || 'blank'];
@@ -20,19 +24,37 @@ function PortfolioItem() {
   ];
 
   return (
-    <PageContainer sx={{ pt: 6, pb: 6 }}>
+    <PageContainer>
       <CustomCard sx={{ alignItems: 'flex-start' }}>
         <Breadcrumb items={portfolioItemBreadcrumb} />
+        <img
+          src={getPortfolioImagePathFromSlug(portfolioItem.slug, currentTheme)}
+          loading="lazy"
+          alt={`${portfolioItem.title} main image`}
+          width="100%"
+          height="auto"
+          style={{
+            objectFit: 'cover',
+            marginBottom: '1rem',
+            width: '100%',
+            maxWidth: '40rem',
+            borderRadius: '4px',
+            border: `1px solid ${addOpacityToColor('#000000', 0.2)}`,
+          }}
+        />
         <Typography sx={{ mb: 0, transition: standardTransitions(theme) }} variant="h1">
           {portfolioItem.title}
         </Typography>
         {portfolioItem.subtitle && (
-          <Typography sx={{ mb: 0, transition: standardTransitions(theme) }} variant="body1">
+          <Typography
+            sx={{ mb: 0, transition: standardTransitions(theme), columnCount: 2 }}
+            variant="body1"
+          >
             {portfolioItem.subtitle}
           </Typography>
         )}
         <Box sx={{ mb: 4 }} />
-        {portfolioItem.content}
+        <Box sx={{ columnCount: { xs: 1, md: 2 }, columnGap: 2 }}>{portfolioItem.content}</Box>
       </CustomCard>
     </PageContainer>
   );
