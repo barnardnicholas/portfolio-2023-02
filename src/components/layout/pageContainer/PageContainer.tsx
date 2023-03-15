186cbe9718bb458e20aaa222ce1366f7c3cf92c4
Nick Barnard <n.barnard@ctidigital.com>
2023-03-15 13:43:49 +0000

    Add default styles to PageContainer

--- src/components/layout/pageContainer/PageContainer.tsx
+++ src/components/layout/pageContainer/PageContainer.tsx
@@ -2,7 +2,7 @@ import { Container, SxProps, Theme } from '@mui/material';
 import React, { PropsWithChildren } from 'react';
 
 const PageContainer: React.FC<PageContainerProps> = ({ children, sx = {} }) => {
-  return <Container sx={{ flex: 1, ...sx }}>{children}</Container>;
+  return <Container sx={{ flex: 1, pt: 10, pb: 6, ...sx }}>{children}</Container>;
 };
 
 interface PageContainerProps extends PropsWithChildren {
